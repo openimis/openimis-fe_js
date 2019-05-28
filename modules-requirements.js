21abd80c4dc873ac8ea0e74e01a6b929d271ae36
Xavier Gillmann <xgi0101@gmail.com>
2019-05-28 14:00:36 +0200

    init all proxies

--- modules-requirements.js
+++ modules-requirements.js
@@ -6,12 +6,18 @@ fs.readFile('./openimis.json', 'utf8', function read(err, data) {
 
     var srcModules = fs.createWriteStream('./src/modules.js');
     var modulesInstalls = fs.createWriteStream('./modules-installs.txt');
+    var modulesRemoves = fs.createWriteStream('./modules-removes.txt');
+    var modulesLinks = fs.createWriteStream('./modules-links.txt');
+    var modulesUnlinks = fs.createWriteStream('./modules-unlinks.txt');
 
     config['modules'].forEach((module) => {        
         let lib = module.npm.substring(0, module.npm.lastIndexOf('@'));
-        srcModules.write(`import { ${module.name} } from '${lib}';\n`);
+        srcModules.write(`import { ${module.name} } from '${lib}';\n`);        
         modulesInstalls.write(`yarn remove ${lib}\n`);
         modulesInstalls.write(`yarn add ${module.npm}\n`);
+        modulesRemoves.write(`yarn remove ${lib}\n`);
+        modulesLinks.write(`yarn link ${lib}\n`);
+        modulesUnlinks.write(`yarn unlink ${lib}\n`);
     })
 
     srcModules.write("\nexport default [\n")
--- src/modules.js
+++
@@ -1,8 +0,0 @@
-import { CoreModule } from '@openimis/fe-core';
-import { InsureeModule } from '@openimis/fe-insuree';
-import { ClaimModule } from '@openimis/fe-claim';
-
-export default [
-CoreModule,
-InsureeModule,
-ClaimModule];
