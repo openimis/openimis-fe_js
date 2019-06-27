2694f134c651fc4b84807349bc4dd4e147785911
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-06-27 20:07:00 +0200

    merge conflict resolution

--- config-overrides.js
+++ config-overrides.js
@@ -5,10 +5,7 @@ module.exports = function override(config, env) {
     alias: {
       'react':  path.resolve('./node_modules/react'),
       'react-redux':  path.resolve('./node_modules/react-redux'),
-<<<<<<< HEAD
       '@material-ui':  path.resolve('./node_modules/@material-ui'),
-=======
->>>>>>> develop
     }
   };
   return config;
