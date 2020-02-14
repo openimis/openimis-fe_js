9df30b845207e8ab1c6c7eb9aeae16611ea2de3a
xa <xgi0101@gmail.com>
2020-02-14 14:54:42 +0100

    OMT-88: IE compatibility

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from "react";
 import ReactDOM from "react-dom";
 import { MuiThemeProvider, LinearProgress } from "@material-ui/core";
