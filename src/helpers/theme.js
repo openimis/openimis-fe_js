31e2640d0b04f04a76138321a7064c7a5e7f1bc9
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-03-20 17:55:20 +0100

    OMT-149: opening claims in new tabs

--- src/helpers/theme.js
+++ src/helpers/theme.js
@@ -94,6 +94,12 @@ const theme = createMuiTheme({
       color: "#006273",
       align: "center",
     },
+    cell: {
+      paddingTop: 5,
+      paddingBottom: 5,
+      paddingLeft: 10,
+      paddingRight: 10,
+    },
     lockedRow: {
       background: "repeating-linear-gradient(45deg, #D3D3D3 1px, #D3D3D3 1px, #fff 10px, #fff 10px);"
     },
@@ -169,3 +175,4 @@ const theme = createMuiTheme({
 });
 
 export default theme;
+
