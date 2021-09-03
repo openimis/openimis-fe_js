60839008307ea80766342a3f7b80fa8bf013ef4e
Quentin Gérôme <geromequentin@gmail.com>
2021-09-03 14:09:14 +0200

    chore: Refactor entry module + Add command to load config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import React from "react";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { MuiThemeProvider, LinearProgress } from "@material-ui/core";
 import { Provider } from "react-redux";
 import MomentUtils from "@date-io/moment";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import * as serviceWorker from "./serviceWorker";
-import history from './helpers/history';
+import history from "./helpers/history";
 import theme from "./helpers/theme";
 import store from "./helpers/store";
 import LocalesManager from "./LocalesManager";
@@ -19,76 +19,80 @@ import "./index.css";
 import logo from "./openIMIS.png";
 import HistoryProvider from "./HistoryProvider";
 
-const fatalError = (resp) => {
-    const app = (
-        <FatalError error={
-            {
-                code: resp.status,
-                message: resp.statusText
-            }
-        } />
-    );
-    ReactDOM.render(app, document.getElementById("root"));
-}
-
-const bootApp = (cfg) => {
-    const cfgs = cfg.data.moduleConfigurations.reduce((cfgs, c) => {
-        try {
-            cfg = JSON.parse(c.config)
-            cfgs[c.module] = { controls: c.controls, ...cfg };
-        } catch (error) {
-            console.error(`Failed to parse module ${c.module} config`, error);
-        }
-        return cfgs
+const loadConfiguration = async () => {
+  const response = await fetch(`${baseApiUrl}/graphql`, {
+    method: "post",
+    headers: apiHeaders(),
+    body: JSON.stringify({ "query": "{ moduleConfigurations { module, config, controls{ field, usage } } }" }),
+  });
+  if (!response.ok) {
+    throw response;
+  } else {
+    const { data } = await response.json();
+    return data.moduleConfigurations.reduce((acc, c) => {
+      try {
+        acc[c.module] = { controls: c.controls, ...JSON.parse(c.config) };
+      } catch (error) {
+        console.error(`Failed to parse module ${c.module} config`, error);
+      }
+      return acc;
     }, []);
-    const localesManager = new LocalesManager();
-    const modulesManager = new ModulesManager(cfgs);
-    const reducers = modulesManager
-        .getContribs('reducers')
-        .reduce((reds, red) => { reds[red.key] = red.reducer; return reds }, []);
-    const app = (
-        <MuiThemeProvider theme={theme}>
-            <Provider store={store(reducers)}>
-                <MuiPickersUtilsProvider utils={MomentUtils}>
-                    <HistoryProvider history={history}>
-                        <ModulesManagerProvider modulesManager={modulesManager}>
-                            <App
-                                localesManager={localesManager}
-                                messages={messages_ref}
-                                logo={logo}
-                            />
-                        </ModulesManagerProvider>
-                    </HistoryProvider>
-                </MuiPickersUtilsProvider>
-            </Provider>
-        </MuiThemeProvider>
+  }
+};
+
+const AppContainer = () => {
+  const [appState, setAppState] = React.useState({ isLoading: true, config: undefined, error: null });
+  const localesManager = new LocalesManager();
+
+  useEffect(() => {
+    loadConfiguration().then(
+      (config) =>
+        setAppState({
+          error: null,
+          isLoading: false,
+          config,
+        }),
+      (error) =>
+        setAppState({
+          error,
+          isLoading: false,
+        })
     );
-    ReactDOM.render(app, document.getElementById("root"));
-}
+  }, []);
 
+  if (appState.isLoading) {
+    return <LinearProgress className="bootstrap" />;
+  } else if (appState.error) {
+    return (
+      <FatalError
+        error={{
+          code: appState.error.status,
+          message: appState.error.statusText,
+        }}
+      />
+    );
+  } else {
+    const modulesManager = new ModulesManager(appState.config);
+    const reducers = modulesManager.getContribs("reducers").reduce((reds, red) => {
+      reds[red.key] = red.reducer;
+      return reds;
+    }, []);
 
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store(reducers)}>
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <HistoryProvider history={history}>
+              <ModulesManagerProvider modulesManager={modulesManager}>
+                <App localesManager={localesManager} messages={messages_ref} logo={logo} />
+              </ModulesManagerProvider>
+            </HistoryProvider>
+          </MuiPickersUtilsProvider>
+        </Provider>
+      </MuiThemeProvider>
+    );
+  }
+};
 
-const payload = `
-{
-    moduleConfigurations { module, config, controls{field, usage}}
-}
-`;
-fetch(`${baseApiUrl}/graphql`,
-    {
-        method: "post",
-        headers: apiHeaders(),
-        body: JSON.stringify({ "query": payload }),
-    }
-).then(resp => {
-    if (!resp.ok) {
-        fatalError(resp);
-    } else {
-        resp.json().then(cfg => bootApp(cfg));
-    }
-});
-
-
-const app = <LinearProgress className="bootstrap" />;
-
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(<AppContainer />, document.getElementById("root"));
 serviceWorker.register();
