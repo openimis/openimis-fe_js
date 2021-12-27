a11d07eb74a7d24fc98d0ae7158edbe85d256ab3
Quentin Gérôme <geromequentin@gmail.com>
2021-12-27 15:42:30 +0100

    fet(Proxy): Only enable Remote-User header if provided as env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,16 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const pkg = require("../package.json");
 
 module.exports = function (app) {
+  let headers = {};
+  if (process.env.REMOTE_USER) {
+    headers["Remote-User"] = process.env.REMOTE_USER;
+  }
   app.use(
     "/api",
     createProxyMiddleware({
       target: pkg.proxy,
       changeOrigin: true,
-      headers: { "Remote-User": "Admin" },
+      headers: headers,
     })
   );
 };
