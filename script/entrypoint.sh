e28c0dfc8c6521d21fc575118592c8ac7a719b88
Patrick Delcroix <delcroip@gmail.com>
2023-02-17 18:17:03 +0100

    woking

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -9,6 +9,7 @@ rm -f /etc/nginx/conf.d/openIMIS.conf
 rm -f /etc/nginx/conf.d/default.conf
 cp  /conf/openimis.conf /etc/nginx/conf.d/openIMIS.conf
 envsubst  '${ROOT_URI},${ROOT_API},${NEW_OPENIMIS_HOST},${ROOT_MOBILEAPI}' < /conf/openimis.conf > /etc/nginx/conf.d/openIMIS.conf
+ln -s -f  /usr/share/nginx/html /usr/share/nginx/html/${ROOT_URI}
 echo "Hosting on https://""$NEW_OPENIMIS_HOST"
 echo "root uri $ROOT_URI"
 echo "root api $ROOT_API"
