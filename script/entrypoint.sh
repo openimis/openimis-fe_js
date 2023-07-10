17d05bb143bbe63f21374f07a8bb8f3f46077747
sniedzielski <52816247+sniedzielski@users.noreply.github.com>
2023-07-10 17:04:36 +0200

    CM-153: use single nginx in gateway for openSearch  (#92)
    * CM-153: added support of openSearch dashboard within openIMIS frontend ecosystem
    
    * CM-153: fixed env examples
    
    * CM-153: changed in env example
    
    * CM-153: use single nginx in gateway for openSearch
    
    * CM-153: remove redundand env variables
    
    * CM-153: removed redundant echo in entrypoint script

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -10,14 +10,16 @@ REF=$(date +'%m%d%Y%p')
 rm -f /etc/nginx/conf.d/openIMIS.confs
 rm -f /etc/nginx/conf.d/default.conf
 cp  /conf/openimis.conf /etc/nginx/conf.d/openIMIS.conf
-envsubst  '${PUBLIC_URL},${REACT_APP_API_URL},${NEW_OPENIMIS_HOST},${ROOT_MOBILEAPI},${REDIRECT_TAIL},${DATA_UPLOAD_MAX_MEMORY_SIZE},${OPENSEARCH_PROXY_HOST},${OPENSEARCH_PROXY_ROOT}' < /conf/openimis.conf > /etc/nginx/conf.d/openIMIS.conf
+# update the conf
+envsubst  '${PUBLIC_URL},${REACT_APP_API_URL},${NEW_OPENIMIS_HOST},${ROOT_MOBILEAPI},${REDIRECT_TAIL},${DATA_UPLOAD_MAX_MEMORY_SIZE},${OPENSEARCH_PROXY_ROOT},${$OPENSEARCH_BASIC_TOKEN}' < /conf/openimis.conf > /etc/nginx/conf.d/openIMIS.conf
+# update the loc
+for f in $(find /conf/location -regex '.*\.loc'); do envsubst '${PUBLIC_URL},${REACT_APP_API_URL},${NEW_OPENIMIS_HOST},${ROOT_MOBILEAPI},${REDIRECT_TAIL},${DATA_UPLOAD_MAX_MEMORY_SIZE},${OPENSEARCH_PROXY_ROOT},${$OPENSEARCH_BASIC_TOKEN}' < $f > "/etc/nginx/conf.d/location/$(basename $f)"; done
+
 ln -s -f  /usr/share/nginx/html /usr/share/nginx/html/${PUBLIC_URL}
 echo "Hosting on https://""$NEW_OPENIMIS_HOST"
 echo "root uri $PUBLIC_URL"
 echo "root api $REACT_APP_API_URL"
 echo "root restapi $ROOT_MOBILEAPI"
 echo "OpenSearch proxy base root $OPENSEARCH_PROXY_ROOT"
-echo "OpenSearch proxy host $OPENSEARCH_PROXY_HOST"
-echo "Proxy host $PROXY_HOST"
 
 exec "$@"
