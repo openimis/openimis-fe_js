a3b91f21d8feea08b12678faaea11e8ca4266330
jobil <j.bilongo@ynote.africa>
2023-08-01 11:37:11 +0100

    resolve conflict

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,17 +6,15 @@ module.exports = function (app) {
   if (process.env.REMOTE_USER) {
     headers["Remote-User"] = process.env.REMOTE_USER;
   }
-<<<<<<< HEAD
+
   console.log("Proxy");
   console.log(pkg.proxy);
-=======
 
   let baseApiUrl = process.env.REACT_APP_API_URL ?? '/api';
   if (baseApiUrl.indexOf('/') !== 0) {
     baseApiUrl = `/${baseApiUrl}`;
   }
 
->>>>>>> 9e68ad33284eb082dbea56359c34cebb5db7582e
   app.use(
     baseApiUrl,
     createProxyMiddleware({
