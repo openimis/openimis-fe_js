acca88212b616a256cf3663c891658519d07568a
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2024-01-31 17:08:32 +0100

    add opensearch token

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -10,7 +10,7 @@ REF=$(date +'%m%d%Y%p')
 rm -f /etc/nginx/conf.d/openIMIS.confs
 rm -f /etc/nginx/conf.d/default.conf
 cp  /conf/openimis.conf /etc/nginx/conf.d/openIMIS.conf
-envsubst  '${PUBLIC_URL},${REACT_APP_API_URL},${NEW_OPENIMIS_HOST},${ROOT_MOBILEAPI},${REDIRECT_TAIL},${DATA_UPLOAD_MAX_MEMORY_SIZE}' < /conf/openimis.conf > /etc/nginx/conf.d/openIMIS.conf
+envsubst  '${PUBLIC_URL},${REACT_APP_API_URL},${NEW_OPENIMIS_HOST},${ROOT_MOBILEAPI},${REDIRECT_TAIL},${DATA_UPLOAD_MAX_MEMORY_SIZE},${$OPENSEARCH_BASIC_TOKEN}' < /conf/openimis.conf > /etc/nginx/conf.d/openIMIS.conf
 ln -s -f  /usr/share/nginx/html /usr/share/nginx/html/${PUBLIC_URL}
 echo "Hosting on https://""$NEW_OPENIMIS_HOST"
 echo "root uri $PUBLIC_URL"
