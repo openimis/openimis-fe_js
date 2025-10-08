563982b67e1a22705c2b1226e15b465e0873839d
delcroip <patrick.delcroix@swisstph.ch>
2025-10-08 13:23:53 +0200

    chore: misc changes

--- script/entrypoint-dev.sh
+++ script/entrypoint-dev.sh
@@ -8,9 +8,8 @@ source ~/.bashrc  # or ~/.zshrc, ~/.bash_profile, etc., depending on your shell
 npm install yargs shelljs
 node ./dev_tools/entrypoint-dev.js -c /app/openimis-dev.json -p /frontend-packages
 echo "Updating package.json"
-npm install  --include=dev
 node ./openimis-config-vite.js -c /app/openimis-dev.json -p /frontend-packages
 echo "Install application"
-npm install 
+npm install  --include=dev
 echo "Application has been updated!, will start now"
-npx vite  --host -- openimis-dev.json
+npm start -- openimis-dev.conf --host
